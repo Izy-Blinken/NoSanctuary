2b7fb4a6d7e0ee85ef3e46ba865644075d40830f
Izy-Blinken <m.sharmainsalamat@gmail.com>
2026-02-15 19:30:31 +0800

    added sample files for other folders to appear

---
+++ src/database/DatabaseConnection.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class DatabaseConnection {
+    
+    public static void main(String[] args) {
+        //sample file lang para mauupload sa github
+    }
+    
+}
---
+++ src/models/models.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class models {
+     public static void main(String[] args) {
+        //sample file lang para mauupload sa github
+    }
+}
