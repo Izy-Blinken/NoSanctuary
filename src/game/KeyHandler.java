919496e4d0d4db12cf03964ee977f21e0fc5f965
dddan31 <danielfrancia802@gmail.com>
2026-03-05 17:05:13 +0800

    Update KeyHandler.java

--- src/game/KeyHandler.java
+++ src/game/KeyHandler.java
@@ -6,16 +6,9 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
 
     public boolean upPressed, downPressed, rightPressed, leftPressed;
-    public boolean interactPressed;
-    public boolean closePressed;
-    
-    public boolean ePressed;
-    
-    public boolean depositPressed = false;
-    public boolean withdrawPressed = false; 
-    public boolean appleDepositPressed = false; 
-    public boolean appleWithdrawPressed = false; 
+    public boolean ePressed, eJustPressed; // E = interact with shelter
 
+    @Override
     public void keyTyped(KeyEvent e) {
 
     }
@@ -40,34 +33,11 @@ public class KeyHandler implements KeyListener {
         if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
             leftPressed = true;
         }
-        
-        if (code == KeyEvent.VK_E ) {
-            interactPressed = true;
+
+        if (code == KeyEvent.VK_E) {
+            if (!ePressed) { eJustPressed = true; }
             ePressed = true;
         }
-        
-        if(code == KeyEvent.VK_F) {
-            closePressed = true;
-        }
-        
-        if(code == KeyEvent.VK_P) {
-            
-            depositPressed = true;
-        }
-        
-        if(code == KeyEvent.VK_R) {
-            withdrawPressed = true;
-        }
-        
-        if(code == KeyEvent.VK_B) {
-            appleDepositPressed = true;
-        }
-        
-        if(code == KeyEvent.VK_C) {
-            appleWithdrawPressed = true;
-        }
-        
-       
     }
 
     @Override
@@ -90,34 +60,12 @@ public class KeyHandler implements KeyListener {
         if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
             leftPressed = false;
         }
-        
-        if (code == KeyEvent.VK_E ) {
-            interactPressed = false;
-            ePressed = false;
+
+        if (code == KeyEvent.VK_E) {
+            ePressed      = false;
+            eJustPressed  = false;
         }
 
-        if(code == KeyEvent.VK_F) {
-            closePressed = false;
-        }
-        
-        if(code == KeyEvent.VK_P) {
-            
-            depositPressed = false;
-        }
-        
-        if(code == KeyEvent.VK_R) {
-            withdrawPressed = false;
-        }
-        
-        if(code == KeyEvent.VK_B) {
-            appleDepositPressed = false;
-        }
-        
-        if(code == KeyEvent.VK_C) {
-            appleWithdrawPressed = false;
-        }
-        
     }
-    
 
 }
