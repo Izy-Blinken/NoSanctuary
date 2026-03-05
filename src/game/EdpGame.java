cfce7838107cc558666d5cc34d4032dcf6fcbc35
shouuueeiii <edrianhalili@gmail.com>
2026-03-05 18:32:38 +0800

    Update EdpGame.java

--- src/game/EdpGame.java
+++ src/game/EdpGame.java
@@ -1,24 +1,38 @@
 package game;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class EdpGame {
-
+    
     public static void main(String[] args) {
-
-        JFrame frame = new JFrame("No Sanctuary");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-
-        panel gamePanel = new panel();
-        frame.add(gamePanel);
-
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        frame.setVisible(true);
-        gamePanel.startThread();
-
+        
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("No Sanctuary");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            
+            LandingPage landingPage = new LandingPage(() -> {
+                
+                frame.getContentPane();
+                
+                panel gamePanel = new panel();
+                frame.add(gamePanel);
+                
+                frame.pack();
+                frame.revalidate();
+                frame.repaint();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                
+                gamePanel.startThread();
+                gamePanel.requestFocusInWindow();
+            });
+            frame.add(landingPage);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+        
     }
 }
